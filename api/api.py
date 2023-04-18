c8298cc260b3903714f36455bc8d6eed71563ff9
Gabriel Teixeira <gabriel.teixeira009@gmail.com>
2023-04-18 18:18:39 -0300

    api
    campos novos em API basica em python

--- api/api.py
+++ api/api.py
@@ -1,1 +1,10 @@
-print('hello world')
+#API 
+#1. Objetivo - criar uma API que disponibiliza a consulta,criação, edição e exclusão de livros
+#2. URL base - localhost
+#3. Endpoints - 
+    # -  localhost/livros (GET)
+    # -  localhost/livros/ (POST)
+    # -  localhost/livros/id (GET)
+    # -  localhost/livros/id (PUT)
+    # -  localhost/livros/id (DELETE)
+#4. Quais recursos - Livros
